2271eae106b148b345845a33483be310ef0244ca
Saswat Panda <sapanda88@gmail.com>
2014-08-11 23:33:29 -0500

    Added Bing Maps solution

--- src/solutions/Main.java
+++ src/solutions/Main.java
@@ -2,7 +2,7 @@ package solutions;
 
 public class Main {
     public static void main(String[] args) {
-        MapQuest m = new MapQuest();
+        BingMaps m = new BingMaps();
         String address = "130 Harvard Ave E, Seattle, WA 98122";
         LatLong latlong = new LatLong(47.6196523, -122.321789839922);
 
