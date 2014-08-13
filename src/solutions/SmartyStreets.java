0a5eed2a1f8ff4927e094bd1bf655f8f0469b031
Maddi <ross.maddi@gmail.com>
2014-08-13 13:08:04 -0500

    Edited SmartyStreets so that it no longer prints url.

--- src/solutions/SmartyStreets.java
+++ src/solutions/SmartyStreets.java
@@ -9,113 +9,113 @@ import org.restlet.data.Reference;
 import org.restlet.ext.json.JsonRepresentation;
 import org.restlet.representation.Representation;
 
-public class SmartyStreets extends Solution{
-	//these is a free trial auth-id and auth-token
-	//limited to 250 lookups per month after September 12, 2014
-    private final String authId = "9110bb3d-0e00-4746-aa42-8cc58cd1f56d";
-    private final String authToken = "s0rBT9aLR6MCPIEB9Cyh";
+public class SmartyStreets extends Solution {
+	// these is a free trial auth-id and auth-token
+	// limited to 250 lookups per month after September 12, 2014
+	private final String authId = "9110bb3d-0e00-4746-aa42-8cc58cd1f56d";
+	private final String authToken = "s0rBT9aLR6MCPIEB9Cyh";
 
-	 @Override
-	 public String normalize(String address) {
-		 String normAddress = "";
+	@Override
+	public String normalize(String address) {
+		String normAddress = "";
 
-	        // Make the query
-	        String url = "https://api.smartystreets.com/street-address";
+		// Make the query
+		String url = "https://api.smartystreets.com/street-address";
 
-	        Reference ref = new Reference(url);
-	        ref.addQueryParameter("street", address);
-	        ref.addQueryParameter("auth-id", authId);
-	        ref.addQueryParameter("auth-token", authToken);
-	        System.out.println(ref);
+		Reference ref = new Reference(url);
+		ref.addQueryParameter("street", address);
+		ref.addQueryParameter("auth-id", authId);
+		ref.addQueryParameter("auth-token", authToken);
 
-	        Representation rep = getRepresentation(ref);
+		Representation rep = getRepresentation(ref);
 
-	        try {
-	            // Parse the Data
-	            JsonRepresentation jr = new JsonRepresentation(rep);
-	            JSONArray jarr = jr.getJsonArray();
+		try {
+			// Parse the Data
+			JsonRepresentation jr = new JsonRepresentation(rep);
+			JSONArray jarr = jr.getJsonArray();
 
-	            // TODO: Deal with multiple return values
-	            if (jarr.length() > 0) {
-	                JSONObject jobj = jarr.getJSONObject(0).getJSONObject("components");
-	                normAddress = parseAddress(jobj);
-	            }
+			// TODO: Deal with multiple return values
+			if (jarr.length() > 0) {
+				JSONObject jobj = jarr.getJSONObject(0).getJSONObject(
+						"components");
+				normAddress = parseAddress(jobj);
+			}
 
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        } catch (JSONException e) {
-	            e.printStackTrace();
-	        }
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
 
-	        return normAddress;
-	  }
+		return normAddress;
+	}
 
-	    @Override
-	    public LatLong geocode(String address) {
-	        LatLong latlong = new LatLong(0, 0);
+	@Override
+	public LatLong geocode(String address) {
+		LatLong latlong = new LatLong(0, 0);
 
-	        // Make the query
-	        String url = "https://api.smartystreets.com/street-address";
+		// Make the query
+		String url = "https://api.smartystreets.com/street-address";
 
-	        Reference ref = new Reference(url);
-	        ref.addQueryParameter("street", address);
-	        ref.addQueryParameter("auth-id", authId);
-	        ref.addQueryParameter("auth-token", authToken);
-	        System.out.println(ref);
+		Reference ref = new Reference(url);
+		ref.addQueryParameter("street", address);
+		ref.addQueryParameter("auth-id", authId);
+		ref.addQueryParameter("auth-token", authToken);
 
-	        Representation rep = getRepresentation(ref);
+		Representation rep = getRepresentation(ref);
 
-	        try {
-	            // Parse the Data
-	            JsonRepresentation jr = new JsonRepresentation(rep);
-	            JSONArray jarr = jr.getJsonArray();
+		try {
+			// Parse the Data
+			JsonRepresentation jr = new JsonRepresentation(rep);
+			JSONArray jarr = jr.getJsonArray();
 
-	            if (jarr.length() > 0) {
-	                JSONObject jobj = jarr.getJSONObject(0).getJSONObject("metadata");
-	                latlong.latitude = jobj.getDouble("latitude");
-	                latlong.longitude = jobj.getDouble("longitude");
-	            }
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        } catch (JSONException e) {
-	            e.printStackTrace();
-	        }
+			if (jarr.length() > 0) {
+				JSONObject jobj = jarr.getJSONObject(0).getJSONObject(
+						"metadata");
+				latlong.latitude = jobj.getDouble("latitude");
+				latlong.longitude = jobj.getDouble("longitude");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
 
-	        return latlong;
-	    }
+		return latlong;
+	}
 
-	    @Override
-	    public String reverseGeocode(LatLong latlong) {
-	        //doesn't do reverse geocoding
-	        return null;
-	    }
+	@Override
+	public String reverseGeocode(LatLong latlong) {
+		// doesn't do reverse geocoding
+		return null;
+	}
 
-	    private String parseAddress(JSONObject jobj) throws JSONException {
-	        String address = "";
-	        //for normalization
-	        if (jobj.has("primary_number")){
-	        	address+=jobj.getString("primary_number")+" ";
-	        }
-	        if (jobj.has("street_predirection")) {
-	            address += jobj.getString("street_predirection") + " ";
-	        }
-	        if (jobj.has("street_name")) {
-	            address += jobj.getString("street_name") + " ";
-	        }
-	        if (jobj.has("street_suffix")) {
-	            address += jobj.getString("street_suffix") + ", ";
-	        }
-	        if (jobj.has("city_name")) {
-	            address += jobj.getString("city_name") + ", ";
-	        }
-	        if (jobj.has("state_abbreviation")) {
-	            address += jobj.getString("state_abbreviation") + ", ";
-	        }
-	        if (jobj.has("zipcode")) {
-	            address += jobj.getString("zipcode") + " ";
-	        }
+	private String parseAddress(JSONObject jobj) throws JSONException {
+		String address = "";
+		// for normalization
+		if (jobj.has("primary_number")) {
+			address += jobj.getString("primary_number") + " ";
+		}
+		if (jobj.has("street_predirection")) {
+			address += jobj.getString("street_predirection") + " ";
+		}
+		if (jobj.has("street_name")) {
+			address += jobj.getString("street_name") + " ";
+		}
+		if (jobj.has("street_suffix")) {
+			address += jobj.getString("street_suffix") + ", ";
+		}
+		if (jobj.has("city_name")) {
+			address += jobj.getString("city_name") + ", ";
+		}
+		if (jobj.has("state_abbreviation")) {
+			address += jobj.getString("state_abbreviation") + ", ";
+		}
+		if (jobj.has("zipcode")) {
+			address += jobj.getString("zipcode") + " ";
+		}
 
-	        return address;
-	    }
+		return address;
+	}
 
 }
