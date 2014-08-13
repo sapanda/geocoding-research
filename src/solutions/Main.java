be8539b5fdbcf3436050f7e30abe8440618862c3
Maddi <ross.maddi@gmail.com>
2014-08-13 03:26:32 -0500

    Added MapLarge.

--- src/solutions/Main.java
+++ src/solutions/Main.java
@@ -4,7 +4,7 @@ package solutions;
 public class Main {
     public static void main(String[] args) {
         SmartyStreets m = new SmartyStreets();
-        String address = "5490 South Shore Dr, Chicago IL 60615";
+        String address = "5490 South Shore Ave, Chicago, IL 60615";
 
         System.out.println("Normalize: " + m.normalize(address));
 
---
+++ src/solutions/MapLarge.java
@@ -0,0 +1,56 @@
+package solutions;
+
+import java.io.IOException;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.restlet.data.Reference;
+import org.restlet.ext.json.JsonRepresentation;
+import org.restlet.representation.Representation;
+import org.restlet.resource.ClientResource;
+
+public class OpenStreetMaps implements Solution {
+	private String key = "dummyKey";
+    @Override
+    public String normalize(String address) {
+        return null;
+    }
+
+    @Override
+    public LatLong geocode(String address) {
+        LatLong latlong = new LatLong(0, 0);
+
+        // Make the query
+        String url = "http://geocoder.maplarge.com/geocoder/json?";
+        Reference ref = new Reference(url);
+        ref.addQueryParameter("address", address);
+        ref.addQueryParameter("key", key);
+
+        Representation rep = new ClientResource(ref).get();
+
+        try {
+            // Parse the Data
+            JsonRepresentation jr = new JsonRepresentation(rep);
+            JSONArray jarr = jr.getJsonArray();
+
+            if (jarr.length() > 0) {
+                JSONObject jobj = jarr.getJSONObject(0);
+                latlong.latitude = jobj.getDouble("lat");
+                latlong.longitude = jobj.getDouble("LNG");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return latlong;
+    }
+
+    @Override
+    public String reverseGeocode(LatLong latlong) {
+    
+        return null;
+    }
+}
